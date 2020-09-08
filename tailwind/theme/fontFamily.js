ba3ebe64d43a1c986c68ce7f01ff3b9ceba4d15e
infelipeq13 <70453031+infelipeq13@users.noreply.github.com>
2020-09-08 18:30:05 -0300

    Change Playfair Display font to .woff2

--- tailwind/theme/fontFamily.js
+++ tailwind/theme/fontFamily.js
@@ -2,5 +2,5 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   sans: ["Poppins", ...defaultTheme.fontFamily.sans],
-  serif: ["Playfair Display", ...defaultTheme.fontFamily.serif],
+  serif: ['"Playfair Display"', ...defaultTheme.fontFamily.serif],
 };
